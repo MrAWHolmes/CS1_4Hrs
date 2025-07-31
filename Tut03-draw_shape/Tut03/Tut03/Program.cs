19774c72facb786415c1b2801f1d54ff4ff5b883
MrAWHolmes <archie.holmes@gmail.com>
2025-07-31 17:25:15 +0100

    tut03A - vs

---
+++ Tut03-draw_shape/Tut03/Tut03/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ref : https://asciiset.com/
+
+static void main()
+{
+    Dictionary<string,char> table = new Dictionary<string, char>();
+    table.Add("TOP_LEFT", '╔');
+    table.Add("TOP", '═');
+    table.Add("TOP_RIGHT", '╗');
+    table.Add("LEFT", '║');
+    table.Add("RIGHT", '║');
+    table.Add("BOTTOM_LEFT", '╚');
+    table.Add("BOTTOM", '═');
+    table.Add("BOTTOM_RIGHT", '╝');
+
+    string text = "Hello, World!";
+
+    // draw top
+    Console.Write($"{table["TOP_LEFT"]}");
+    for(int i = 0;i<text.Length + 2; i++) 
+    {
+        Console.Write($"{table["TOP"]}");
+    }
+    Console.WriteLine($"{table["TOP_RIGHT"]}");
+
+    // draw text
+    Console.WriteLine($"{table["LEFT"]} {text} {table["RIGHT"]}");
+
+    //draw the bottom
+    Console.Write($"{table["BOTTOM_LEFT"]}");
+    for (int i = 0; i < text.Length + 2; i++)
+    {
+        Console.Write($"{table["TOP"]}");
+    }
+    Console.WriteLine($"{table["BOTTOM_RIGHT"]}");
+
+
+    Console.ReadLine();
+}
+
+main();
---
+++ readme.md
@@ -0,0 +1,11 @@
+REf: https://youtube.com/playlist?list=PLLAZ4kZ9dFpNIBTYHNDrhfE9C-imUXCmk&si=vIn0t3GBb-z4pxYO
+
+C# - Programming Language | Tutorial
+by Giraffe Academy
+
+TUT02 - Hello World https://youtu.be/samkJ9LOMWY?si=UxGE3POzo5Snm5lj
+    Reading :   (1) https://learn.microsoft.com/en-gb/dotnet/core/tutorials/top-level-templates 
+                (2) https://learn.microsoft.com/en-us/dotnet/core/tutorials/with-visual-studio-code  
+
+TUT03 - Drawing a shape https://youtu.be/hw90WDwk-xY?list=PLLAZ4kZ9dFpNIBTYHNDrhfE9C-imUXCmk
+
