807da14c5ef4d64f1045c33029fc42aaf5f22f00
MrAWHolmes <archie.holmes@gmail.com>
2025-07-31 16:46:10 +0100

    Tut02

---
+++ Tut02-firstprogram/Tutorial02/Tut02/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* implicit using directives: 
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+ 
+Global using directives
+A global using directive imports a namespace for your whole 
+application instead of a single file. These global directives 
+can be added either by adding a <Using> item to the project file, 
+or 
+by adding the global using directive to a code file.
+
+ */
+Console.WriteLine("Hello, World!");
