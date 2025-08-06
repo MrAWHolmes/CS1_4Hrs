8d0c11bfcb878b6624aeff7d77b97d6133d9c71d
MrAHolmes <archie.holmes@gmail.com>
2025-08-06 16:55:09 +0100

    tut06 strings interpolated strings and split & join

---
+++ tut06-strings/tut06-strings/tut06-strings/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace tut06_strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "This is a string y'all!";
+
+
+            Console.WriteLine($"message = {message}");
+            Console.WriteLine($"message.ToUpper() = {message.ToUpper()}");
+            Console.WriteLine($"message.ToLower() = {message.ToLower()}");
+            Console.WriteLine($"message.Length = {message.Length}");
+            Console.WriteLine($"message.Contains(\"string\") = {message.Contains("string")}");
+            Console.WriteLine($"message.IndexOf(\"string\" = {message.IndexOf("string")}");
+            Console.WriteLine($"message.Substring(5) = {message.Substring(5)}");
+            Console.WriteLine($"message.Substring(message.Length-5) = {message.Substring(message.Length-5)}");
+
+            Console.WriteLine($"{{string.Join(\", \",message.Split(' '))}} = {{{string.Join(", ",message.Split(' '))}}}");
+
+        }
+    }
+}
