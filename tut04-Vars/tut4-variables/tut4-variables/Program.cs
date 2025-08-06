3f0fa97596142be2dce39074584b2f659b41cddd
MrAHolmes <archie.holmes@gmail.com>
2025-08-06 11:54:32 +0100

    Tut04-variables

---
+++ tut04-Vars/tut4-variables/tut4-variables/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tut4_variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adult = "woman";
+            string name = "Lilly";
+            string proNoun = "She";
+            int age = 90;
+
+            Console.WriteLine($"There once was a {adult} named {name},");
+            Console.WriteLine($"\t{proNoun} was {age} years old.");
+            Console.WriteLine($"{proNoun} really liked the name {name},");
+            Console.WriteLine($"\tbut didn't like being {age}!");
+
+            Console.ReadLine();
+        }
+    }
+}
