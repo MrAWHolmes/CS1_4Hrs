df3fbd92347979f8c3c202100a648923596b7d58
MrAHolmes <archie.holmes@gmail.com>
2025-08-07 23:56:47 +0100

    tut13b - simple delegates experiment. basic and lambda work butexternal class method does not

---
+++ Tut13b-DelegatesEg1/DelegatesExample1/DelegatesExample1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DelegatesExample1
+{
+    //delegate should be declared outside the class
+    public delegate void MyDelegate(string msg); // declare the delegate
+
+    internal class Program
+    {
+
+        static TaDa TaDaObject;
+
+        static void LocalMethod(string msg)
+        {
+            Console.WriteLine("You called LocalMethod:");
+            Console.WriteLine(msg);
+            Console.WriteLine();
+        }
+
+        static MyDelegate LambdaFunc = (string msg) =>
+        {
+            Console.WriteLine("Im the lambda function:");
+            Console.WriteLine(msg);
+            Console.WriteLine();
+        };
+
+        static void Main(string[] args)
+        {
+            MyDelegate[] Callbacks = {LocalMethod,LambdaFunc,  TaDaObject.SayHi };
+
+            for (int i = 0; i < Callbacks.Length; i++)
+            {
+                Callbacks[i]($"Called from Callable[{i}]");
+            }
+        }
+
+        class TaDa
+        {
+            public static void sayHi(string msg)
+            {
+                Console.WriteLine("Hello from class TaDa!");
+                Console.WriteLine(msg);
+                Console.WriteLine();
+            }
+        }
+    }
+}
