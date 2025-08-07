09eafe71d4e8194764f5deb79ceb8c9d2d3b149c
MrAHolmes <archie.holmes@gmail.com>
2025-08-07 10:44:51 +0100

    tut08-user intput with cool ebook link

---
+++ tut08-user_input/Tut08-UserInput/Tut08-UserInput/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Tut08_UserInput
+{ /*
+   *  Cool Reference : https://csharp-book.softuni.org
+   */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            Console.Write("What is your name? ");
+            name = Console.ReadLine();
+
+            Console.Write("How old are you? ");
+            age = int.Parse(Console.ReadLine());
+
+            Console.Write("What is your GPA? ");
+            var gpa = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name + " you are " + age + " years old.");
+            Console.WriteLine("You have a gpa of " + gpa);
+            
+        }
+    }
+}
