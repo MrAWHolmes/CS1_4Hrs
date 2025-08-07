5985a34a0cac8834ece6ffdb4102c0ff216ca681
MrAHolmes <archie.holmes@gmail.com>
2025-08-07 10:55:37 +0100

    tut09-calc 1 fun fact about c.WriteLine for str()+int()+int()

---
+++ tut09-calculator/tut09-calc1/tut09-calc1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace tut09_calc1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+
+
+            Console.Write("Enter integer 1 : ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter integer 2 : ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            // fun fact! if now () around addition num1+num2 it concatenates!
+
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1+num2));
+
+            Console.ReadLine();
+        }
+    }
+}
