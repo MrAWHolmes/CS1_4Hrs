5100fca663d3ee72c0735d00b12708106e4dc3ae
MrAHolmes <archie.holmes@gmail.com>
2025-08-07 22:08:30 +0100

    tut13-returns coded functionally

---
+++ tut13-returns/Tut13-Returns/Tut13-Returns/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Tut13_Returns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                Console.WriteLine($"{i,3}^3 = {cube(i),5}");
+            }
+
+            Console.ReadKey();  
+        }
+
+        static int times(int a, int b)
+        {
+            return a * b;
+        }
+        static int cube(int num)
+        {
+            return times(times(num, num), num);
+        }
+    }
+}
