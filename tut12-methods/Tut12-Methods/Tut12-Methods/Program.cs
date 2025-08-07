e99314326662d55817e2f3f9e40583056ad84989
MrAHolmes <archie.holmes@gmail.com>
2025-08-07 21:49:41 +0100

    tut12-methods. Note declrarion can be below Main()!

---
+++ tut12-methods/Tut12-Methods/Tut12-Methods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Tut12_Methods
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Fred");
+
+            Console.ReadKey();
+        }
+
+        static void SayHi(string name)
+        {
+            Console.WriteLine("Hello , "+name);
+        }
+    }
+}
