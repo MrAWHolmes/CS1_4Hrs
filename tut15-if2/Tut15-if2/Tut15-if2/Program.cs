bb9c821908c6f6b5f6f7c84b3072f35416651f95
MrAHolmes <archie.holmes@gmail.com>
2025-08-08 12:51:26 +0100

    tut15-if2 max function. i did funcitonal style and max of 3

---
+++ tut15-if2/Tut15-if2/Tut15-if2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using System.Threading.Channels;
+
+namespace Tut15_if2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = ReadInt("Enter value for A");
+            int B = ReadInt("Enter value for B");
+            int C = ReadInt("Enter value for C");
+
+            int maxValue = GetMaxInt(GetMaxInt(A, B), C);
+
+            Console.WriteLine($"GetMaxInt({A},{B},{C}) = {maxValue}");
+        }
+
+
+        static int ReadInt(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            return int.Parse(Console.ReadLine());
+        }
+        static int GetMaxInt(int A, int B)
+        {
+            if (A > B) return A; else return B;
+
+        }
+    }
+}
